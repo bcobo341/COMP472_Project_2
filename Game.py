accf3d6780d817e09c9c70c4e98a53d8c3d36318
Wilson Wei Chen Huang <48188884+willyyhuang@users.noreply.github.com>
2021-11-13 15:33:05 -0500

    init

--- Game.py
+++ Game.py
@@ -8,59 +8,100 @@ class Game:
 	HUMAN = 2
 	AI = 3
 	
-	def __init__(self, recommend = True):
+	def __init__(self, recommend = True, board_size = 3, bloc_num=0, blocs_positions=[], win_size = 3):
+		self.board_size = board_size
+		self.bloc_num=bloc_num
+		self.blocs_positions=blocs_positions
+		self.win_size = win_size if (win_size <= board_size) else board_size
 		self.initialize_game()
 		self.recommend = recommend
 		
-	def initialize_game(self):
-		self.current_state = [['.','.','.'],
-							  ['.','.','.'],
-							  ['.','.','.']]
+	def initialize_game(self): 
+		#self.current_state = [['.','.','.'],
+		#					  ['.','.','.'],
+		#					  ['.','.','.']]
+		# initialize the game board to an nxn square of '.'
+		self.current_state = [ ['.']*self.board_size for i in range(self.board_size)]
+		for bloc in self.blocs_positions:
+			self.current_state[int(bloc[0])][int(bloc[2])] = '$'
+
 		# Player X always plays first
 		self.player_turn = 'X'
 
 	def draw_board(self):
 		print()
-		for y in range(0, 3):
-			for x in range(0, 3):
+		for y in range(0, self.board_size):
+			for x in range(0, self.board_size):
 				print(F'{self.current_state[x][y]}', end="")
 			print()
 		print()
 		
 	def is_valid(self, px, py):
-		if px < 0 or px > 2 or py < 0 or py > 2:
+		if px < 0 or px > (self.board_size-1) or py < 0 or py > (self.board_size-1):
 			return False
 		elif self.current_state[px][py] != '.':
 			return False
 		else:
 			return True
 
+	def is_diagonal_win(self):
+		n = self.board_size
+		s = self.win_size
+		win = False
+		# loop over all squares
+		for x in range(n):
+			for y in range(n):
+				char = self.current_state[x][y]
+				if (char == '$'):
+					# no point in checking the diagonal from a block
+					continue
+
+				# check if a large enough positive diagonal can stem from this square
+				if ((n - x >= s) and (n - y >= s)): 
+					# check positive diagonal on this square
+					win = True
+					for i in range(s):
+						win = win and (self.current_state[x + i][y + i] == char)
+					if win:
+						return char
+
+				# check if a large enough positive diagonal can stem from this square	
+				if ((n - x >= s) and (y >= s - 1)):
+					# check negative diagonal on this square
+					win = True
+					for j in range(s):
+						win = win and (self.current_state[x + j][y - j] == char)
+					if win:
+						return char
+		return '.'
+
 	def is_end(self):
+		win_X = 'X'*self.win_size
+		win_O = 'O'*self.win_size
 		# Vertical win
-		for i in range(0, 3):
-			if (self.current_state[0][i] != '.' and
-				self.current_state[0][i] == self.current_state[1][i] and
-				self.current_state[1][i] == self.current_state[2][i]):
-				return self.current_state[0][i]
-		# Horizontal win
-		for i in range(0, 3):
-			if (self.current_state[i] == ['X', 'X', 'X']):
+		for i in range(0, self.board_size):
+			column = [item[i] for item in self.current_state]
+			column_string = "".join(str(x) for x in column)
+			if (win_X in column_string):
 				return 'X'
-			elif (self.current_state[i] == ['O', 'O', 'O']):
+			if (win_O in column_string):
 				return 'O'
-		# Main diagonal win
-		if (self.current_state[0][0] != '.' and
-			self.current_state[0][0] == self.current_state[1][1] and
-			self.current_state[0][0] == self.current_state[2][2]):
-			return self.current_state[0][0]
-		# Second diagonal win
-		if (self.current_state[0][2] != '.' and
-			self.current_state[0][2] == self.current_state[1][1] and
-			self.current_state[0][2] == self.current_state[2][0]):
-			return self.current_state[0][2]
+		# Horizontal win
+		for i in range(0, self.board_size):
+			row_string = "".join(str(x) for x in self.current_state[i])
+			if (win_X in row_string):
+				return 'X'
+			if (win_O in row_string):
+				return 'O'
+				
+		# Diagonal Win
+		diagonal_result = is_diagonal_win(self)
+		if (diagonal_result != '.'):
+			return diagonal_result
+
 		# Is whole board full?
-		for i in range(0, 3):
-			for j in range(0, 3):
+		for i in range(0, self.board_size):
+			for j in range(0, self.board_size):
 				# There's an empty field, we continue the game
 				if (self.current_state[i][j] == '.'):
 					return None
@@ -219,10 +260,45 @@ class Game:
 			self.current_state[x][y] = self.player_turn
 			self.switch_player()
 
+	def heuristic1_eval(self):
+		e1=0
+		temp=0
+		for y in range(0,self.board_size):
+			for x in range(0,self.board_size):
+				if(temp>=0 and self.current_state[y][x]=='X'):
+					temp+=1
+				if(temp<=0 and self.current_state[y][x]=='0'):
+					temp-=1
+				if(self.current_state[y][x]=='$'):
+					break
+			
+
 def main():
-	g = Game(recommend=True)
+	n = int(input('enter the size of the board n: '))
+	b = int(input('enter the number of blocs b: '))
+	s = int(input('enter the winning line-up size s: '))
+	t = int(input('enter the maximum allowed time (in seconds) t: '))
+
+	d1 = int(input('Player 1, enter maximum depth d1: '))
+	d2 = int(input('Player 2, enter maximum depth d2: '))
+
+	a1 = bool(input('enter either minimax (0) or alphabeta (1) a1: '))
+	a2 = bool(input('enter either minimax (0) or alphabeta (1) a2: '))
+
+	p1 = int(input('enter either AI (2) or Human (3) p1: '))
+	p2 = int(input('enter either AI (2) or Human (3) p2: '))
+
+	blocPositions = []
+	for bloc in range(0, b):
+		bx = int(input('enter the bloc position x: '))
+		by = int(input('enter the bloc position y: '))
+		blocPositions.append(f'{bx} {by}')
+
+	#g = Game(recommend=True)
+	g = Game(recommend=True, board_size = n,bloc_num=b, blocs_positions=blocPositions, win_size = s)
+	#g.play(algo=Game.ALPHABETA,player_x=p1,player_o=p2)
 	g.play(algo=Game.ALPHABETA,player_x=Game.AI,player_o=Game.AI)
-	g.play(algo=Game.MINIMAX,player_x=Game.AI,player_o=Game.HUMAN)
+	#g.play(algo=Game.MINIMAX,player_x=Game.AI,player_o=Game.HUMAN)
 
 if __name__ == "__main__":
 	main()
