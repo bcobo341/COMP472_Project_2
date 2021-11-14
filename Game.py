36d83d3f8b9b5dcd221ccf6fe09e38f18a947ea1
Wilson Wei Chen Huang <48188884+willyyhuang@users.noreply.github.com>
2021-11-14 13:38:37 -0500

    update minimax

--- Game.py
+++ Game.py
@@ -1,5 +1,5 @@
 import time
-import numpy as p
+import numpy as np
 
 class Game:
     MINIMAX = 0
@@ -7,13 +7,15 @@ class Game:
     AI = 2
     HUMAN = 3
     
-    def __init__(self, recommend = True, board_size = 3, bloc_num=0, blocs_positions=[], win_size = 3, t = 0):
+    def __init__(self, recommend = True, board_size = 3, bloc_num=0, blocs_positions=[], win_size = 3, t = 0, max_depth_X=0, max_depth_O=0):
         self.recommend = recommend
         self.board_size = board_size
         self.bloc_num=bloc_num
         self.blocs_positions=blocs_positions
         self.win_size = win_size if (win_size <= board_size) else board_size
         self.t = t
+        self.max_depth_X = max_depth_X
+        self.max_depth_O = max_depth_O
         self.initialize_game()
         
     def initialize_game(self): 
@@ -155,17 +157,68 @@ class Game:
         elif self.player_turn == 'O':
             self.player_turn = 'X'
         return self.player_turn
+        
+    def minimax(self, max=False, current_depth = 0):
+        # Maximizing for 'X' and minimizing for 'O'
+        # Possible values are:
+        # 10^win_size - win for 'X'
+        # 0  - a tie
+        # -10^win_size  - loss for 'X'
+        # We're initially setting it to 2*10^win_size or -2*10^win_size as worse than the worst case:
+        max_depth = self.max_depth_O
+        value = -2*pow(10, self.win_size)
 
-    def minimax(self, max=False):
+        if max:
+            value = 2*pow(10, self.win_size)
+            max_depth = self.max_depth_X
+
+        x = None
+        y = None
+        
+        if current_depth == max_depth:
+            result = self.is_end()
+            if result == 'X':
+                return (1*pow(10, self.win_size), x, y)
+            elif result == 'O':
+                return (-1*pow(10, self.win_size), x, y)
+            elif result == '.':
+                return (0, x, y)
+            elif result == None:
+                return (self.heuristic2_eval(), x, y)
+
+        # no result, calculate for heuristic value when max_depth is reached
+        for i in range(0, self.board_size):
+
+            for j in range(0, self.board_size):
+                if self.current_state[i][j] == '.':
+                    if max:
+                        self.current_state[i][j] = 'O'
+                        (v, _, _) = self.minimax(max=False, current_depth=current_depth+1)
+                        if v < value:
+                            value = v
+                            x = i
+                            y = j
+                    else:
+                        self.current_state[i][j] = 'X'
+                        (v, _, _) = self.minimax(max=True, current_depth=current_depth+1)
+                        if v > value:
+                            value = v
+                            x = i
+                            y = j
+                    self.current_state[i][j] = '.'
+        return (value, x, y)
+
+    def alphabeta(self, alpha=-2, beta=2, max=False):
         # Minimizing for 'X' and maximizing for 'O'
         # Possible values are:
-        # -1 - win for 'X'
+        # 10^win_size - win for 'X'
         # 0  - a tie
-        # 1  - loss for 'X'
-        # We're initially setting it to 2 or -2 as worse than the worst case:
-        value = 2
+        # -10^win_size  - loss for 'X'
+        # We're initially setting it to 2*10^win_size or -2*10^win_size as worse than the worst case:
+
+        value = -2*pow(10, self.win_size)
         if max:
-            value = -2
+            value = 2*pow(10, self.win_size)
         x = None
         y = None
         result = self.is_end()
@@ -177,45 +230,6 @@ class Game:
             return (0, x, y)
         for i in range(0, self.board_size):
             for j in range(0, self.board_size):
-                if self.current_state[i][j] == '.':
-                    if max:
-                        self.current_state[i][j] = 'O'
-                        (v, _, _) = self.minimax(max=False)
-                        if v > value:
-                            value = v
-                            x = i
-                            y = j
-                    else:
-                        self.current_state[i][j] = 'X'
-                        (v, _, _) = self.minimax(max=True)
-                        if v < value:
-                            value = v
-                            x = i
-                            y = j
-                    self.current_state[i][j] = '.'
-        return (value, x, y)
-
-    def alphabeta(self, alpha=-2, beta=2, max=False):
-        # Minimizing for 'X' and maximizing for 'O'
-        # Possible values are:
-        # -1 - win for 'X'
-        # 0  - a tie
-        # 1  - loss for 'X'
-        # We're initially setting it to 2 or -2 as worse than the worst case:
-        value = 2
-        if max:
-            value = -2
-        x = None
-        y = None
-        result = self.is_end()
-        if result == 'X':
-            return (-1, x, y)
-        elif result == 'O':
-            return (1, x, y)
-        elif result == '.':
-            return (0, x, y)
-        for i in range(0, 3):
-            for j in range(0, 3):
                 if self.current_state[i][j] == '.':
                     if max:
                         self.current_state[i][j] = 'O'
@@ -260,9 +274,6 @@ class Game:
             trace_file.write("Player X: " + str(player_x) + "\n")
             trace_file.write("Player Y: " + str(player_o) + "\n")
 
-        # default algo
-        if algo == None:
-            algo = self.ALPHABETA
         # default players if not specified
         if player_x == None:
             player_x = self.HUMAN
@@ -306,9 +317,6 @@ class Game:
 
     # Heuristic 2: more sophisticated and complex to compute
     def heuristic2_eval(self):
-        self.current_state = [['X','.','X'],
-                              ['.','O','.'],
-                              ['X','.','.']]
         e2=0
         tempx=0
         tempo=0
@@ -358,8 +366,8 @@ class Game:
                 e2+=pow(10,tempx)
             tempx=0
             tempo=0
-
         print(F'e2 = {e2}')
+        return e2
 
 def main():
     n = int(input('enter the size of the board n: '))
@@ -370,11 +378,11 @@ def main():
     d1 = int(input('Player 1, enter maximum depth d1: '))
     d2 = int(input('Player 2, enter maximum depth d2: '))
 
-    a1 = bool(input('enter either minimax (0) or alphabeta (1) for game 1: '))
-    a2 = bool(input('enter either minimax (0) or alphabeta (1) for game 2: '))
+    a1 = int(input('enter either minimax (0) or alphabeta (1) for game 1: '))
+    a2 = int(input('enter either minimax (0) or alphabeta (1) for game 2: '))
 
-    p1 = int(input('enter either AI (2) or Human (3) p1: '))
-    p2 = int(input('enter either AI (2) or Human (3) p2: '))
+    p1 = int(input('Player 1, enter either AI (2) or Human (3) p1: '))
+    p2 = int(input('Player 2, enter either AI (2) or Human (3) p2: '))
 
     blocPositions = []
     for _ in range(0, b):
@@ -382,11 +390,9 @@ def main():
         by = int(input('enter the bloc position y: '))
         blocPositions.append(f'{bx} {by}')
 
-    g = Game(recommend=True, board_size=n, bloc_num=b, blocs_positions=blocPositions, win_size=s)
+    g = Game(recommend=True, board_size=n, bloc_num=b, blocs_positions=blocPositions, win_size=s, max_depth_X=d1, max_depth_O=d2, t=t)
     g.play(algo=a1, player_x=p1, player_o=p2)
-
-    g = Game(recommend=True, board_size = n,bloc_num=b, blocs_positions=blocPositions, win_size = s, t=t)
-    g.play(algo=a2,player_x=p1,player_o=p2)
+    # g.play(algo=a2,player_x=p1,player_o=p2)
 
 if __name__ == "__main__":
     main()
